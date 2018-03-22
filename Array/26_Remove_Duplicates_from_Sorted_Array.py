ea6a2655aab9ad746cf1fd274fe59d700f88846c
RichardKxy <37448195+RichardKxy@users.noreply.github.com>
2018-03-22 16:49:45 -0500

    Add files via upload

--- Array/26_Remove_Duplicates_from_Sorted_Array.py
+++ Array/26_Remove_Duplicates_from_Sorted_Array.py
@@ -10,3 +10,15 @@ class Solution:
             if nums[len(nums)-2] == nums[len(nums)-1]:
                 nums.pop()
         return len(nums)
+
+"""public int removeDuplicates(int[] nums) {
+    if (nums.length == 0) return 0;
+    int i = 0;
+    for (int j = 1; j < nums.length; j++) {
+        if (nums[j] != nums[i]) {
+            i++;
+            nums[i] = nums[j];
+        }
+    }
+    return i + 1;
+}"""
